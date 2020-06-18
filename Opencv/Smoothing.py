f18a213b89feab581fd457a29d367565cae7d577
Darkie <darkie@Kali.localhost>
2020-06-18 09:22:37 +0530

    file name changed

--- Opencv/Smoothing.py
+++ Opencv/Smoothing.py
@@ -2,7 +2,8 @@ import cv2
 from matplotlib import pyplot as plt 
 import numpy as np 
 
-img = cv2.imread("lena.jpg")
+img = cv2.imread("saltandpepper.png")
+print(img)
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
 kernel = np.ones((5,5),np.float32)
@@ -10,7 +11,7 @@ kernelvalue = np.ones((5,5),np.float32)/25
 filter2d = cv2.filter2D(img,-1,kernel)
 gaussianblur = cv2.GaussianBlur(img,kernel,0,0)
 medianblur = cv2.medianBlur(img,5)
-bilateralFilter = cv2.bilateralFilter(img,kernelvalue75,75)
+bilateralFilter = cv2.bilateralFilter(img,kernelvalue,75,75)
 
 outputs = [img,filter2d,gaussianblur,medianblur,bilateralFilter]
 titles = ["Original","Filtered2D","Gaussian Blur","MedianBlur","BilateralFilter"]
