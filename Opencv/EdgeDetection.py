54a30d222b001486c61bb038c4abcbd537287209
Darkie <darkie@Kali.localhost>
2020-06-18 19:33:26 +0530

    Canny edge Detextion Method Included

--- Opencv/EdgeDetection.py
+++ Opencv/EdgeDetection.py
@@ -10,11 +10,12 @@ sobelx = np.uint8(np.absolute(sobelx))
 sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=1)
 sobely = np.uint8(np.absolute(sobely))
 sobelxy = cv2.bitwise_or(sobelx,sobely)
+canny = cv2.Canny(img,100,200)
 
-images = [img,lap,sobelx,sobely,sobelxy]
-titles = ["Original","Laplacian","SobelX","SobelY","SobelXY"]
+images = [img,lap,sobelx,sobely,sobelxy,canny]
+titles = ["Original","Laplacian","SobelX","SobelY","SobelXY","Canny"]
 
-for i in range(5):
+for i in range(6):
     plt.subplot(2,3,i+1),plt.imshow(images[i],"gray")
     plt.xticks([]),plt.yticks([])
     
